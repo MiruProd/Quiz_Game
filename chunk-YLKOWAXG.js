0df320484f0abe82008430050d57f216b26ccdf1
MiruProd <mistik201400@gmail.com>
2026-06-08 16:53:24 +0000

    Deploying to gh-pages from @ MiruProd/Quiz_Game@2cbc58b72af6ee64178c1a0733349c5f480a6f7f 🚀

---
+++ chunk-YLKOWAXG.js
@@ -0,0 +1,1 @@
+import{Ha as s,Ia as n,Na as a,ta as i}from"./chunk-ZXZE3UE4.js";var o=class e{static \u0275fac=function(t){return new(t||e)};static \u0275cmp=i({type:e,selectors:[["app-quiz-list"]],decls:2,vars:0,template:function(t,l){t&1&&(s(0,"p"),a(1,"quiz-list works!"),n())},encapsulation:2})};export{o as QuizList};
