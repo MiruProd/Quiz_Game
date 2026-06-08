522fe71bc4891045ff51493186eb6c839355ea50
MiruProd <225598651+MiruProd@users.noreply.github.com>
2026-06-09 01:25:35 +0700

    Создан базовый ApiService

---
+++ src/app/services/api.service.ts
@@ -0,0 +1,31 @@
+import { Injectable, inject } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ApiService {
+  private readonly http = inject(HttpClient);
+  private readonly baseUrl = 'https://milesis-quiz-game-backend.hf.space';
+
+  /* Выполнение GET запроса */
+  get<T>(path: string, params?: HttpParams): Observable<T> {
+    return this.http.get<T>(`${this.baseUrl}/${path}`, { params });
+  }
+
+  /* Выполнение POST запроса */
+  post<T>(path: string, body: unknown): Observable<T> {
+    return this.http.post<T>(`${this.baseUrl}/${path}`, body);
+  }
+
+  /* Выполнение PUT запроса */
+  put<T>(path: string, body: unknown): Observable<T> {
+    return this.http.put<T>(`${this.baseUrl}/${path}`, body);
+  }
+
+  /* Выполнение DELETE запроса */
+  delete<T>(path: string): Observable<T> {
+    return this.http.delete<T>(`${this.baseUrl}/${path}`);
+  }
+}
